a3c2f53f1615b7c86456db36fb7673bffbb560bf
berlinum <maks.filimonov@gmail.com>
2020-03-13 11:56:54 +0100

    Add button componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Button from './components/Button';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <main className="main">
+        <Button />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css';
+
+function Button() {
+  return <button className="btn">BUTTON</button>;
+}
+
+export default Button;
